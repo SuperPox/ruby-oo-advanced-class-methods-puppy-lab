c9cb45ce2a0962946fc9ee3f850029c93499e9be
SuperPox <calebkohl@gmail.com>
2021-01-22 12:06:53 -0700

    commit tree e5f1e05ec6afcc3af9fed70f51290195cf211aa5

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
-# Add your code here
+require 'pry'
+class Dog
+    attr_accessor :name 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+        #@@all << self  #need to do this in "save method"
+        #self.save this is wrong 
+    end
+
+    def self.all
+        @@all
+    end
+
+    def Dog.clear_all
+        @@all.clear
+    end
+
+    def Dog.print_all
+        #iterates over all Dog instances and puts their name to terminal
+        @@all.each do |dogs|
+            puts "#{dogs.name}"
+        end
+    end
+
+    def save
+        @@all << self
+    end
+end
